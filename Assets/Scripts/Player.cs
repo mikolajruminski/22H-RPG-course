c9cf19c51041334bdfc17db7328369557d48046f
mikolajruminski <56116082+mikolajruminski@users.noreply.github.com>
2023-11-15 16:11:10 +0100

    Lesson 21

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
     private PlayerInputActions playerInputActions;
     private Rigidbody2D playerRB;
     [SerializeField] private float playerSpeedMultiplier;
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
         playerRB = GetComponent<Rigidbody2D>();
@@ -16,7 +26,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
